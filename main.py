b3700267efce5778fd3d4acfaa9a4c8f708e12a7
nialish-arshard-10p <m.usman@careem.com>
2021-10-04 16:51:45 +0500

    [Refactored] modules for test cases

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+import logging
 from resources.google_analytics import GoogleAnalytics
-from unit_tests.google_analytics import TestGoogleAnalytics
 import unittest
+import argparse
 from argparse import ArgumentParser
 
 if __name__ == "__main__":
 
-   parser = ArgumentParser(description='Argument parser for PG restore')
-   parser.add_argument('--full_visitor_id', default=None, required=True, help='fullvisitorId to process data')
-   parser.add_argument('--run_test', default=None, type=bool, required=False, help='Hyper parameter to validate test result')
+   parser = ArgumentParser(description='argument parser for analytics')
+   parser.add_argument('--full_visitor_id', default=False, required=True, help='fullvisitorId to process data')
+   parser.add_argument('--run_test', default=False, action='store_true', help='Hyper parameter to validate test result')
+   parser.set_defaults(run_test=False)
    parsed_args = parser.parse_args()
 
    if parsed_args.run_test:
-      unittest.main()
+      logging.info("Performing Unit Test Cases")
+      suite = unittest.TestLoader().discover('.', pattern = "*_test.py")
+      unittest.TextTestRunner(verbosity=2).run(suite)
 
-   else:
-      response = GoogleAnalytics().process_job_by_visitor_id(parsed_args.run_test)
+   if parsed_args.full_visitor_id:
+      response = GoogleAnalytics().process_job_by_visitor_id(parsed_args.full_visitor_id)
       print(response)
--- unit_tests/google_analytics_test.py
+++ unit_tests/google_analytics_test.py
@@ -8,10 +8,8 @@ class TestGoogleAnalytics(unittest.TestCase):
     def test_process_job_by_visitor_id_no_visitor_id(self):
         ga = GoogleAnalytics()
         self.assertDictEqual(ga.process_job_by_visitor_id(""),{"status": 204, "response_message":"No data found", "response_code":"NO_CONTENT", "data": []})
-        
+
     def test_process_job_by_visitor_id(self):
         ga = GoogleAnalytics()
         self.assertEqual(ga.process_job_by_visitor_id("634908280670541439"),{"status": 200, "response_message":"Success", "response_code":"SUCCESS", "data": mock.ANY })
-
-if __name__ == '__main__':
-    unittest.main()
+    
