9b13bb5ed4b50d8b32b521c9c347c2a393319f61
nialish-arshard-10p <m.usman@careem.com>
2021-10-04 16:05:56 +0500

    [Refactored] main flow for test cases and application response

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-
 from resources.google_analytics import GoogleAnalytics
 from unit_tests.google_analytics import TestGoogleAnalytics
 import unittest
 from argparse import ArgumentParser
 
 if __name__ == "__main__":
-   unittest.main()
+
    parser = ArgumentParser(description='Argument parser for PG restore')
    parser.add_argument('--full_visitor_id', default=None, required=True, help='fullvisitorId to process data')
-   parser.add_argument('--run_test', default=None, required=False, help='Hyper parameter to validate test result')
+   parser.add_argument('--run_test', default=None, type=bool, required=False, help='Hyper parameter to validate test result')
    parsed_args = parser.parse_args()
-   #x=GoogleAnalytics().process_job_by_visitor_id("634908280670541439")
-   # print(x)
+
+   if parsed_args.run_test:
+      unittest.main()
+
+   else:
+      response = GoogleAnalytics().process_job_by_visitor_id(parsed_args.run_test)
+      print(response)
