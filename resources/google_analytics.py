2de419cc7bc704d70aafa360530528dea2a21c33
nialish-arshard-10p <m.usman@careem.com>
2021-10-04 23:38:57 +0500

    [Added] comments for each method

--- resources/google_analytics.py
+++ resources/google_analytics.py
@@ -9,6 +9,8 @@ class GoogleAnalytics:
         self.client = bigquery.Client()
         self.common = Common()
 
+    # the method is initializes the query within the client 
+    # for execution and returns the query job to users to proceed.
     def get_query_job_result(self, query):
         try:    
             logging.info("Querying data from the Dataset to analyze")
@@ -18,6 +20,9 @@ class GoogleAnalytics:
             logging.error("Query failed due to the error below")
             logging.error(e)
 
+    # the method wraps the response result in to desired output format and 
+    # appends the wrapped object in the response array. the method takes query job as input 
+    # and iterates over it to prepare response.
     def get_location_order_analytics(self, query_job):
         response=[]
         for row in query_job:
@@ -30,6 +35,9 @@ class GoogleAnalytics:
             })
         return response
 
+    # the method acts as a central layer which combines all the unit methods 
+    # at one place for the consumer to response the method takes fullVisitorId 
+    # as input and return response array as an output.
     def process_job_by_visitor_id(self, fullvisitorId):
         try:
             logging.info("Querying data from the Dataset to analyze")
